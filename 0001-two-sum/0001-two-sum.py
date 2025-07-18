0604ffba125e9f64e51660c4da504779d1d10b6c
coldrun <53134516+PranavKangane@users.noreply.github.com>
2025-07-17 22:22:18 -0400

    Time: 0 ms (100%), Space: 18.8 MB (50.58%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
-                else:
-                    j+= 1
-            
-    
+        empty = {}
+        for index, value in enumerate(nums):
+            diff = target - value
+            if diff in empty:
+                return [empty[diff], index]
+            empty[value] = index
