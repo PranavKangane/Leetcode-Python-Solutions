8a8bac745286384c84c60a204d8c874afcda3226
coldrun <53134516+PranavKangane@users.noreply.github.com>
2025-11-20 14:12:51 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (69.46%) - LeetSync

---
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.py
@@ -0,0 +1,7 @@
+from math import gcd
+class Solution:
+    def findGCD(self, nums: List[int]) -> int:
+        small = min(nums)
+        large = max(nums)
+        return math.gcd(small,large)
+        
