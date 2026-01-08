f026af31f114eb8de37e26a3647821de6db445bf
coldrun <53134516+PranavKangane@users.noreply.github.com>
2026-01-08 09:36:02 -0500

    Time: 15 ms (42.10%) | Memory: 19.4 MB (7.98%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,15 +1,16 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        smap = {}
-        tmap = {}
+        smap, tmap = {}, {}
 
-        for x in range(len(s)):
-            smap[s[x]] = smap.get(s[x],0)+1
-
-        for x in range(len(t)):
-            tmap[t[x]] = tmap.get(t[x],0)+1
-
-        if smap == tmap:
-            return True
-        else:
+        if len(s) != len(t):
             return False
+        
+        for i in range(len(s)):
+            smap[s[i]] = smap.get(s[i],0) + 1
+            tmap[t[i]] = tmap.get(t[i],0) + 1
+
+        for count in smap:
+            if smap[count] != tmap.get(count,0):
+                return False
+
+        return True
