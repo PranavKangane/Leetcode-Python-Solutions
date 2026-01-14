dfdc9b3d3d9ca51ded700b60813198c1de8e19ba
coldrun <53134516+PranavKangane@users.noreply.github.com>
2026-01-14 11:12:10 -0500

    Time: 6 ms (48.15%) | Memory: 19.2 MB (11.91%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,19 +1,24 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        
-        valdic = {'I':1, 'V':5, 'X': 10, 'L': 50, 'C':100, 'D':500, 'M':1000,
-        'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD':400, 'CM':900}
+
+        romandic = {'I':1 , 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
 
         total = 0
-        for i in range(len(s)-1):
-            if valdic[s[i]] < valdic[s[i+1]]:
-                total -= valdic[s[i]]
+        i = 0
+        while i < len(s):
+            if i + 1 < len(s) and romandic[s[i]] < romandic[s[i+1]]:
+                total += romandic[s[i+1]] - romandic[s[i]]
+                i += 2
             else:
-                total += valdic[s[i]]
-            print(total)
-
-        total += valdic[s[-1]]
+                total += romandic[s[i]]
+                i += 1
 
         return total
+
+        
+
+
+        
+
      
 
