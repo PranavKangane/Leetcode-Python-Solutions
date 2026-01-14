3cd3c49883a257257848e34f42ef59953ae9bd70
coldrun <53134516+PranavKangane@users.noreply.github.com>
2026-01-14 10:48:11 -0500

    Time: 36 ms (69.56%) | Memory: 19.4 MB (8.58%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -6,6 +6,7 @@ class Solution:
             if i < len(word1):
                 merged += word1[i]
             if i < len(word2):
-                merged += word2[i] 
-            i += 1
+                merged += word2[i]
+            i+= 1
+
         return merged
